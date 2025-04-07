28bef18817ebc0fe313d27eee353986c0485f742
amolbarkale <barkaleamol@gmail.com>
2025-04-07 20:12:17 +0530

    test memory allocation for primitive data types inside heap memory.

--- extra/test.js
+++ extra/test.js
@@ -4,3 +4,17 @@ let y = 20;
 let z = x + y;
 
 // node --print-bytecode test.js
+
+function memoryUsage(){
+    const mbUsed = process.memoryUsage().heapUsed / 1024 / 1024
+    console.log(`Memory used: ${mbUsed} MB`, )
+}
+
+console.log('Before:')
+memoryUsage()
+
+const bigString = "x".repeat(10 * 1024 * 1024)
+console.log('bigString:', bigString)
+
+console.log('After:')
+memoryUsage()
